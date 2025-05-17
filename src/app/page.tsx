d605d085e36c24ae1a7daca4c4c2087476730b76
Vedanti3o1 <vedanti@taggbox.com>
2025-05-17 15:33:02 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                  src="/portfolio-preview.png"
+                  src="/test.jpg"
                   alt="App screenshot"
                   width={2432}
                   height={1442}
