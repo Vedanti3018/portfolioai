e90b28c76f5cde3a9ef1d764a2c7d916663a6758
Vedanti_3018 <vedanti3018@gmail.com>
2025-06-01 19:08:06 +0530

    fix: add missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -135,8 +135,10 @@ module.exports = {
   	}
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
     require('@tailwindcss/typography'),
-      require("tailwindcss-animate")
+    require("tailwindcss-animate")
 ],
 }
